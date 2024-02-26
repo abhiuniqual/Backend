a557132ed7d5a161030d65726e759b703648d3a1
abhi.uniqual <abhi.uniqual@gmail.com>
2024-02-26 14:42:28 +0530

    feat: added forget-password/verfiy-otp/reset-password

--- auth.js
+++ auth.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
 const knex = require("./knexfile");
 const express = require("express");
 const bcrypt = require("bcrypt");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
-// const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
+const fs = require("fs");
+const util = require("util");
+const readFile = util.promisify(fs.readFile);
 
 app.use(express.json());
 
@@ -82,68 +85,39 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
-// Change password API
-app.post("/api/change-password", async (req, res) => {
-  const { token, email, oldPassword, newPassword } = req.body;
-
-  if (!token || !email || !oldPassword || !newPassword) {
-    return res.status(400).json({ error: "All fields are required" });
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await knex("users").where({ email }).first();
-
-    if (!user || user.id !== decoded.userId) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
-
-    const passwordMatch = await bcrypt.compare(oldPassword, user.password);
-    if (!passwordMatch) {
-      return res.status(401).json({ error: "Invalid password" });
-    }
-
-    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
-    await knex("users")
-      .where({ email })
-      .update({ password: hashedNewPassword });
-
-    res.status(200).json({ message: "Password changed successfully" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
 // Logout API
 app.post("/api/logout", (req, res) => {
   res.status(200).json({ message: "Logout successful" });
 });
 
-// Function to sent OTP
-// const sendOTPByEmail = async (email, otp) => {
-//   try {
-//     let transporter = nodemailer.createTransport({
-//       service: "Gmail",
-//       auth: {
-//         user: "abhi.uniqual@gmail.com",
-//         pass: "",
-//       },
-//     });
+const sendOTPByEmail = async (email, otp) => {
+  try {
+    const htmlTemplate = await readFile("EmailTemplate.html", "utf8");
 
-//     let info = await transporter.sendMail({
-//       from: '"abhi.uniqual@gmail.com',
-//       to: email,
-//       subject: "Password Reset OTP",
-//       text: `Your OTP for password reset is: ${otp}`,
-//     });
+    let transporter = nodemailer.createTransport({
+      service: process.env.SERVICE,
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASS,
+      },
+      tls: {
+        rejectUnauthorized: true,
+      },
+    });
 
-//     console.log("Message sent: %s", info.messageId);
-//   } catch (error) {
-//     console.error("Error occurred while sending email:", error);
-//     throw error;
-//   }
-// };
+    let info = await transporter.sendMail({
+      from: process.env.USER,
+      to: email,
+      subject: "Verify Your Account - Your One-Time Password (OTP)",
+      html: htmlTemplate.replace("{{otp}}", otp).replace("{{time_limit}}", 10),
+    });
+
+    console.log("Message sent: %s", info.messageId);
+  } catch (error) {
+    console.error("Error occurred while sending email:", error);
+    throw error;
+  }
+};
 
 //forget-password
 const generateOTP = () => {
@@ -168,7 +142,67 @@ app.post("/api/forget-password", async (req, res) => {
     const otp = generateOTP();
     const otpExpiration = Date.now() + 10 * 60 * 1000;
     otpData[email] = { otp, otpExpiration };
+
+    await sendOTPByEmail(email, otp);
+
     res.status(200).json({ message: "OTP sent successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// verify-otp
+app.post("/api/verify-otp", (req, res) => {
+  const { email, otp } = req.body;
+
+  if (!email || !otp) {
+    return res.status(400).json({ error: "Email and OTP are required" });
+  }
+
+  const userData = otpData[email];
+
+  if (!userData) {
+    return res.status(400).json({ error: "User data not found" });
+  }
+
+  if (userData.otp !== otp) {
+    return res.status(400).json({ error: "Invalid OTP" });
+  }
+
+  const currentTime = Date.now();
+  if (currentTime > userData.otpExpiration) {
+    return res.status(400).json({ error: "OTP expired" });
+  }
+
+  res.status(200).json({ message: "OTP verified successfully", email });
+});
+
+// reset-password
+app.post("/api/reset-password", async (req, res) => {
+  const { email, newPassword } = req.body;
+
+  if (!email || !newPassword) {
+    return res
+      .status(400)
+      .json({ error: "Email and new password are required" });
+  }
+
+  const userData = otpData[email];
+
+  if (!userData || Date.now() > userData.otpExpiration) {
+    return res
+      .status(400)
+      .json({ error: "OTP expired, please request a new one" });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    await knex("users").where({ email }).update({ password: hashedPassword });
+
+    delete otpData[email];
+
+    res.status(200).json({ message: "Password reset successfully" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
